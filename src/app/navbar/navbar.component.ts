8e6c2b4cd06fcde1b2718605627010fd4dc3c428
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-04-29 21:49:28 +0100

    add navbar html and Add @angular/animations to fix missing dependency error

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ RouterLink, FormsModule ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  searchQuery: string = '';
+  onSearch() {
+    console.log('Search Query:', this.searchQuery);
+  }
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs'; 
+// we use json server to contact the httpClient
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
+  private apiUrl = 'http://localhost:3000/recipes'; // URL del JSON Server
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getRecipes(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getRecipeById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
 }
